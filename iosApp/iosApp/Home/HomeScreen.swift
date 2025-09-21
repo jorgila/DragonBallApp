5f16c6ee5108e4612df6e87664a204f862e3f496
Jorge Gimeno <jgimenolatas@gmail.com>
2025-09-21 10:41:46 +0200

    Configuring Koin in iOS

---
+++ iosApp/iosApp/Home/HomeScreen.swift
@@ -0,0 +1,18 @@
+//
+//  HomeScreen.swift
+//  iosApp
+//
+//  Created by Jorge Gimeno Latas on 21/9/25.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeScreen()
+}
